48896653db8d3ddba723ff74ae7833852f55ea60
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-19 13:18:51 +0200

    zeus: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: xiaomi Model: 2201122G Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_zeus-user
+add_lunch_combo omni_zeus-userdebug
+add_lunch_combo omni_zeus-eng
