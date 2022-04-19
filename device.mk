1073633b5663855d9a302797dcc3b09c9336a934
Droneship <81514881+Seifood@users.noreply.github.com>
2022-04-19 15:08:24 +0200

    nuke

--- device.mk
+++ device.mk
@@ -26,12 +26,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.taro
 
-#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.taro \
-    libgptutils \
-    libz \
-    libcutils
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
