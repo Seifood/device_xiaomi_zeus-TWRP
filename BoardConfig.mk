9396985e3097c838ea80055e643a3d3e94b9cbbe
Droneship <81514881+Seifood@users.noreply.github.com>
2022-04-21 05:44:40 +0200

    Zeus: Kang from psyche

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,7 +62,20 @@ AB_OTA_PARTITIONS += \
     vendor_boot
 
 # Kernel
-VENDOR_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 disable_dma32=on bootinfo.fingerprint=zeus_global:12/V13.0.5.0.SLBMIXM:user mtdoops.fingerprint=zeus_global:12/V13.0.5.0.SLBMIXM:user bootconfig
+VENDOR_CMDLINE := "console=ttyMSM0,115200n8 \
+		androidboot.hardware=qcom \
+		androidboot.console=ttyMSM0 \
+		androidboot.memcg=1 \
+		lpm_levels.sleep_disabled=1 \
+		video=vfb:640x400,bpp=32,memsize=3072000 \
+		msm_rtb.filter=0x237 \
+		service_locator.enable=1 \
+		androidboot.usbcontroller=a600000.dwc3 \
+		swiotlb=2048 \
+		loop.max_part=7 \
+		cgroup.memory=nokmem,nosocket \
+		reboot=panic_warm \
+		androidboot.init_fatal_reboot_target=recovery"
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_OFFSET := 0x00008000
