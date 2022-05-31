9728537146411664768552c14c4df2879a44b8ab
Droneship <81514881+Seifood@users.noreply.github.com>
2022-05-31 07:09:01 -0700

    libs: import much more

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,7 +107,7 @@ PLATFORM_VERSION := 16.1.0
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
-TW_NO_SCREEN_BLANK := true
+TW_NO_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_NO_CPU_TEMP := true 
